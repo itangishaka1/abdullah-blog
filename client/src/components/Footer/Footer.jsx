618a421928a368007047f8b990c328cdf1bc648e
itangishaka1 <abdullahitangishaka@gmail.com>
2022-10-05 16:16:48 -0400

    Footer design

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,9 +1,17 @@
 import './Footer.scss'
 
-
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='footer'>
+      <div className='footer__logo'>
+        I
+        <span className='cycle'>
+          ta<span className='red'>B</span>lo
+        </span>
+        g
+      </div>
+      <span className='footer__copy'>&copy;2022 - itangishaka.com</span>
+    </footer>
   )
 }
 
