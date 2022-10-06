84b1b2d2ea6403a83760cf931dd2f3c5416d0841
itangishaka1 <abdullahitangishaka@gmail.com>
2022-10-06 12:24:59 -0400

    Add new post Page Design

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Register from './pages/Register'
 import SinglePost from './pages/SinglePost/SinglePost'
-import WritePost from './pages/WritePost'
+import WritePost from './pages/WritePost/WritePost'
 import Login from './pages/Login/Login'
 import Layout from './components/Layout/Layout'
 
