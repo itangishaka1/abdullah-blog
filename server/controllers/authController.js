c63e66e6c034d0bdfc55c8037eff985e6808761b
itangishaka1 <abdullahitangishaka@gmail.com>
2022-10-13 13:02:35 -0400

    Logout using Cookies

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -48,11 +48,16 @@ export const login = (req, res) => {
 
     res
       .cookie('access_token', token, {
-        httpOnly: true,
+        httpOnly: true
       })
       .status(200)
       .json(other)
   })
 }
 
-export const logout = (req, res) => {}
+export const logout = (req, res) => {
+  res.clearCookie('access_token', {
+    sameSite: "none",
+    secure: true
+  }).status(200).json("User has been logged out.")
+}
