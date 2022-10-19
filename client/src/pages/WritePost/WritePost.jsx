5960ecb65d43759e7001ede811f79de99554de6e
itangishaka1 <abdullahitangishaka@gmail.com>
2022-10-19 09:49:53 -0400

    How to Add new Item

--- client/src/pages/WritePost/WritePost.jsx
+++ client/src/pages/WritePost/WritePost.jsx
@@ -4,11 +4,23 @@ import 'react-quill/dist/quill.snow.css'
 import { useState } from 'react'
 
 const WritePost = () => {
-  const [value, setValue] = useState('')
+  const [value, setValue] = useState('') // description
+  const [title, setTitle] = useState('')
+  const [img, setImg] = useState(null)
+  const [cat, setCat] = useState('')
+
+  const handleSubmit = async (e) =>{
+    e.preventDefault()
+    
+  }
   return (
     <section className='write-post'>
       <div className='write-post__content'>
-        <input type='text' placeholder='Title' />
+        <input
+          type='text'
+          placeholder='Title'
+          onChange={(e) => setTitle(e.target.value)}
+        />
         <div className='write-post__editor-container'>
           <ReactQuill
             className='write-post__editor'
@@ -27,41 +39,82 @@ const WritePost = () => {
           <span>
             <b>Visibility: </b> Public
           </span>
-          <input style={{ display: 'none' }} type='file' id='file' />
+          <input
+            style={{ display: 'none' }}
+            type='file'
+            id='file'
+            onChange={(e) => setImg(e.target.files[0])}
+          />
           <label className='file' htmlFor='file'>
             Upload Image
           </label>
           <div className='buttons'>
             <button>Save as a draft</button>
-            <button>Update</button>
+            <button onClick={handleSubmit}>Publish</button>
           </div>
         </div>
         <div className='write-post__menu-item'>
           <h1>Category</h1>
           <div className='cat'>
-            <input type='radio' name='cat' value='art' id='art' />
+            <input
+              type='radio'
+              name='cat'
+              value='art'
+              id='art'
+              onChange={(e) => setCat(e.target.value)}
+            />
             <label htmlFor='art'>Art</label>
           </div>
           <div className='cat'>
-            <input type='radio' name='cat' value='science' id='science' />
+            <input
+              type='radio'
+              name='cat'
+              value='science'
+              id='science'
+              onChange={(e) => setCat(e.target.value)}
+            />
             <label htmlFor='science'>Science</label>
           </div>
 
           <div className='cat'>
-            <input type='radio' name='cat' value='technology' id='technology' />
+            <input
+              type='radio'
+              name='cat'
+              value='technology'
+              id='technology'
+              onChange={(e) => setCat(e.target.value)}
+            />
             <label htmlFor='technology'>Technology</label>
           </div>
 
           <div className='cat'>
-            <input type='radio' name='cat' value='cinema' id='cinema' />
+            <input
+              type='radio'
+              name='cat'
+              value='cinema'
+              id='cinema'
+              onChange={(e) => setCat(e.target.value)}
+            />
             <label htmlFor='cinema'>Cinema</label>
           </div>
           <div className='cat'>
-            <input type='radio' name='cat' value='design' id='design' />
+            <input
+              type='radio'
+              name='cat'
+              value='design'
+              id='design'
+              onChange={(e) => setCat(e.target.value)}
+            />
             <label htmlFor='design'>Design</label>
           </div>
           <div className='cat'>
-            <input type='radio' name='cat' value='food' id='food' />
+            <input
+              type='radio'
+              name='cat'
+              value='food'
+              id='food'
+              onChange={(e) => setCat(e.target.value)}
+            />
             <label htmlFor='food'>Food</label>
           </div>
         </div>
